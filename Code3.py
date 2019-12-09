df5e654ff13841253ec48ba853ab75908131201a
Ishani  Madhuwanthi <ishmadhuwanthi@gmail.com>
2019-12-09 18:28:07 +0530

    Delete Code3.py

--- Code3.py
+++
@@ -1,8 +0,0 @@
-#Accept string from the user and display only those characters which are present at an even index
-
-word = str(input("Enter String: "))
-print('Original String is',word)
-print('Printing only even index chars ')
-l = len(word)
-for i in range (l):
-	print(word[i])
